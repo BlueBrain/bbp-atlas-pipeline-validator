41828e72df5a31fa3042304f2b89b537df2f5edd
Leonardo Cristella <leonardo.cristella@epfl.ch>
2024-09-24 13:04:33 +0000

    Drop 'logger' dependency

--- pipeline_validator/pipeline_validator.py
+++ pipeline_validator/pipeline_validator.py
@@ -1,5 +1,5 @@
 import json
-from logger import logger
+from pipeline_validator.logger import logger
 from pipeline_validator.validator.rule_validator import RuleValidator
 from pipeline_validator.validator.schema_validator import SchemaValidator
 
